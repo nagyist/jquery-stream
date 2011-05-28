104e26e12acebd33df7b973dec6358525f428507
flowersinthesand <flowersinthesand@d3f0bfe9-4edb-0784-f560-4b5892850fe9>
2011-05-28 12:07:26 +0000

    prepared WebSocket and HTTP Streaming example running on Jetty 8

--- src/main/java/flowersinthesand/example/ChatServlet.java
+++ src/main/java/flowersinthesand/example/ChatServlet.java
@@ -2,12 +2,15 @@ package flowersinthesand.example;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.Map.Entry;
+import java.util.Queue;
 import java.util.UUID;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
 import javax.servlet.AsyncContext;
@@ -15,31 +18,44 @@ import javax.servlet.AsyncEvent;
 import javax.servlet.AsyncListener;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.eclipse.jetty.websocket.WebSocket;
+import org.eclipse.jetty.websocket.WebSocketServlet;
+
 import com.google.gson.Gson;
 
-@WebServlet(urlPatterns = "/chat", asyncSupported = true)
-public class ChatServlet extends HttpServlet {
+public class ChatServlet extends WebSocketServlet {
 
-	private static final long serialVersionUID = -2919167206889576860L;
-	private Map<String, AsyncContext> contexts = new ConcurrentHashMap<String, AsyncContext>();
+	private static final long serialVersionUID = -8823775068689773674L;
+
+	/* Common */
 	private BlockingQueue<String> messages = new LinkedBlockingQueue<String>();
 	private Thread notifier = new Thread(new Runnable() {
 		public void run() {
 			boolean done = false;
 			while (!done) {
-				String message = null;
 				try {
-					message = messages.take();
-					for (Entry<String, AsyncContext> entry : contexts.entrySet()) {
+					String message = messages.take();
+					for (AsyncContext asyncContext : asyncContexts.values()) {
 						try {
-							sendMessage(entry.getValue().getResponse().getWriter(), message);
+							// Message
+							PrintWriter writer = asyncContext.getResponse().getWriter();
+							writer.print(message.length());
+							writer.print(";");
+							writer.print(message);
+							writer.print(";");
+							writer.flush();
 						} catch (IOException e) {
-							contexts.remove(entry.getKey());
+							asyncContexts.values().remove(asyncContext);
+						}
+					}
+					for (WebSocket webSocket : webSockets) {
+						try {
+							((ChatWebSocket) webSocket).connection.sendMessage(message);
+						} catch (Exception e) {
+							webSockets.remove(webSocket);
 						}
 					}
 				} catch (InterruptedException e) {
@@ -48,15 +64,6 @@ public class ChatServlet extends HttpServlet {
 			}
 		}
 	});
-	
-	private void sendMessage(PrintWriter writer, String message) {
-		// Sends a message according to the message format(message-size ; message-data ;) 
-		writer.print(message.length());
-		writer.print(";");
-		writer.print(message);
-		writer.print(";");
-		writer.flush();
-	}
 
 	@Override
 	public void init(ServletConfig config) throws ServletException {
@@ -64,57 +71,63 @@ public class ChatServlet extends HttpServlet {
 		notifier.start();
 	}
 
+	@Override
+	public void destroy() {
+		messages.clear();
+		webSockets.clear();
+		asyncContexts.clear();
+		notifier.interrupt();
+	}
+
+	/* HTTP Streaming */
+	private Map<String, AsyncContext> asyncContexts = new ConcurrentHashMap<String, AsyncContext>();
+
 	// GET method is used to open stream
 	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException,
-			IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		response.setCharacterEncoding("utf-8");
-		
-		// Sets the Content-Type header to 'text/plain' that is the only one satisfying all streaming transport
+
+		// Content-Type header
 		response.setContentType("text/plain");
-		
-		// For XDomainRequest transport, sets the Access-Control-Allow-Origin header
+
+		// Access-Control-Allow-Origin header
 		response.setHeader("Access-Control-Allow-Origin", "*");
 
-		// Prints the stream connection id for further communication and one kilobyte padding at the top of the response
-		// each part must end with a semicolon
 		PrintWriter writer = response.getWriter();
-		
-		// UUID is very suitable
+
+		// Id
 		final String id = UUID.randomUUID().toString();
 		writer.print(id);
 		writer.print(';');
-		
-		// The padding is needed by XMLHttpRequest of WebKit, XDomainRequest and Hidden iframe transport
+
+		// Padding
 		for (int i = 0; i < 1024; i++) {
 			writer.print(' ');
 		}
 		writer.print(';');
 		writer.flush();
-		
-		System.out.println(id + ": open");
-		
-		// Starts asynchronous mode
+
 		final AsyncContext ac = request.startAsync();
 		ac.setTimeout(5 * 60 * 1000);
 		ac.addListener(new AsyncListener() {
 			public void onComplete(AsyncEvent event) throws IOException {
-				contexts.remove(id);
+				asyncContexts.remove(id);
 			}
 
 			public void onTimeout(AsyncEvent event) throws IOException {
-				contexts.remove(id);
+				asyncContexts.remove(id);
 			}
 
 			public void onError(AsyncEvent event) throws IOException {
-				contexts.remove(id);
+				asyncContexts.remove(id);
 			}
 
 			public void onStartAsync(AsyncEvent event) throws IOException {
 
 			}
 		});
-		contexts.put(id, ac);
+		asyncContexts.put(id, ac);
 	}
 
 	// POST method is used to handle data sent by user through the stream
@@ -123,24 +136,12 @@ public class ChatServlet extends HttpServlet {
 			throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
 
-		// POST request always has metadata parameters
-		
-		// The stream connection id generated by the server 
-		String id = request.getParameter("metadata.id");
-		
-		// Request type such as send and close
-		String type = request.getParameter("metadata.type");
-		
-		System.out.println(id + ": " + type);
-		
-		// Handles a special case
-		if ("close".equals(type)) {
-			// Closes connection by its id
-			AsyncContext ac = contexts.get(id);
+		if ("close".equals(request.getParameter("metadata.type"))) {
+			AsyncContext ac = asyncContexts.get(request.getParameter("metadata.id"));
 			if (ac != null) {
 				ac.complete();
 			}
-			
+
 			return;
 		}
 
@@ -151,16 +152,54 @@ public class ChatServlet extends HttpServlet {
 
 		try {
 			messages.put(new Gson().toJson(data));
-		} catch (Exception e) {
+		} catch (InterruptedException e) {
 			throw new IOException(e);
 		}
 	}
 
+	/* WebSocket */
+	private Queue<WebSocket> webSockets = new ConcurrentLinkedQueue<WebSocket>();
+
 	@Override
-	public void destroy() {
-		messages.clear();
-		contexts.clear();
-		notifier.interrupt();
+	public WebSocket doWebSocketConnect(HttpServletRequest request, String protocol) {
+		return new ChatWebSocket();
+	}
+
+	class ChatWebSocket implements WebSocket.OnTextMessage {
+
+		Connection connection;
+
+		@Override
+		public void onOpen(Connection connection) {
+			this.connection = connection;
+			webSockets.add(this);
+		}
+
+		@Override
+		public void onClose(int closeCode, String message) {
+			webSockets.remove(this);
+		}
+
+		@Override
+		public void onMessage(String queryString) {
+			Map<String, String> data = new LinkedHashMap<String, String>();
+			for (String parameter : queryString.split("&")) {
+				String[] entities = parameter.split("=");
+				try {
+					data.put(URLDecoder.decode(entities[0], "utf-8"),
+							URLDecoder.decode(entities[1], "utf-8"));
+				} catch (UnsupportedEncodingException e) {
+					throw new RuntimeException(e);
+				}
+			}
+
+			try {
+				messages.put(new Gson().toJson(data));
+			} catch (InterruptedException e) {
+				throw new RuntimeException(e);
+			}
+		}
+
 	}
 
 }
